a9c7d284600ef17b0e630f63b939b0a1331b75c5
Thierry Martinez <Thierry.Martinez@inria.fr>
2021-10-18 17:05:49 +0200

    Add missing file and delete previous C program

---
+++ pyml_arch.ml.pp
@@ -0,0 +1,20 @@
+#if __APPLE__
+  #define PLATFORM_NAME Mac
+#elif __linux__
+  #define PLATFORM_NAME Linux
+#elif defined(WIN32) || defined(_WIN32)
+  #define PLATFORM_NAME Windows
+  #define WIN_HANDLE_FD
+#else
+  #error "Unknown platform"
+#endif
+
+type t = Linux | Windows | Mac
+
+let os = PLATFORM_NAME
+
+#if WIN_HANDLE_FD
+  external fd_of_int : int -> Unix.file_descr = "win_handle_fd"
+#else
+  external fd_of_int : int -> Unix.file_descr = "%identity"
+#endif
