bd452fa0f3a75c2fe8acc4035437858e801ad293
Thierry Martinez <Thierry.Martinez@inria.fr>
2022-09-09 22:50:45 +0200

    Compile --with-openssl

--- dockerfiles/python-3.11.0rc1/compile-python-3.11.0rc1.sh
+++ dockerfiles/python-3.11.0rc1/compile-python-3.11.0rc1.sh
@@ -1,11 +1,12 @@
 set -ex
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive \
-  apt-get install --yes wget xz-utils gcc pkg-config make zlib1g-dev
+  apt-get install --yes wget xz-utils gcc pkg-config make zlib1g-dev \
+    libssl-dev libffi-dev
 wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0rc1.tar.xz
 tar -xf Python-3.11.0rc1.tar.xz
 cd Python-3.11.0rc1
-./configure --enable-optimizations --enable-shared
+./configure --enable-optimizations --enable-shared --with-openssl
 make
 sudo make install
-#sudo pip3 install numpy
+sudo LD_LIBRARY_PATH=/usr/local/lib pip3 install numpy
